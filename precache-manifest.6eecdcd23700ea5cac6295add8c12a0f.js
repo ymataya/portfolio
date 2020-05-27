92f5e80d9a887af775720b5a612b29318e425692
Yna Mataya <ylmataya@yahoo.com>
2020-05-27 16:45:07 -0500

    Updates

---
+++ precache-manifest.6eecdcd23700ea5cac6295add8c12a0f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e40262af2ae0c7062987ad5b386b04",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b33bc3b7a427d38bf51",
+    "url": "/portfolio/static/css/main.d0117e82.chunk.css"
+  },
+  {
+    "revision": "04c25cf59c70a9189019",
+    "url": "/portfolio/static/js/2.0a50fd08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0a50fd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b33bc3b7a427d38bf51",
+    "url": "/portfolio/static/js/main.8c7257b9.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "99d29a10fcc116c46368f93bcb8a10be",
+    "url": "/portfolio/static/media/burger.99d29a10.png"
+  },
+  {
+    "revision": "ecd7b8f5c4fe4fa5bf334483cd501d08",
+    "url": "/portfolio/static/media/hamburger.ecd7b8f5.png"
+  },
+  {
+    "revision": "a534fa206e7a4b74251434d8fbb78177",
+    "url": "/portfolio/static/media/me.a534fa20.jpg"
+  },
+  {
+    "revision": "98943a958e3da8a97ecf7ff7e094c2c9",
+    "url": "/portfolio/static/media/planner-1.98943a95.png"
+  },
+  {
+    "revision": "ce8ecd1ca7357edd297d3b4f612f9f20",
+    "url": "/portfolio/static/media/planner.ce8ecd1c.jpg"
+  },
+  {
+    "revision": "251b6fb5ff677fb3f69680f2f00bc199",
+    "url": "/portfolio/static/media/red.251b6fb5.png"
+  },
+  {
+    "revision": "235a25ba0397e734e80d39c94953a661",
+    "url": "/portfolio/static/media/shelter-landing.235a25ba.png"
+  },
+  {
+    "revision": "8a47a9812390a065a787af1b7db574c4",
+    "url": "/portfolio/static/media/shelter-th-01.8a47a981.png"
+  },
+  {
+    "revision": "ecc09887a596ab4eb769a37b4f813eb9",
+    "url": "/portfolio/static/media/stat-quest.ecc09887.jpg"
+  },
+  {
+    "revision": "2e0b2bf823f534de20414b0429a1029b",
+    "url": "/portfolio/static/media/weather-1.2e0b2bf8.png"
+  },
+  {
+    "revision": "6154e1d5a68becc6eaba22cc1a373c81",
+    "url": "/portfolio/static/media/weather.6154e1d5.jpg"
+  }
+]);
